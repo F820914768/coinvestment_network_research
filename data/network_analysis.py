d6dc6c75d79b978d9a1cf8d3ce4d93a26549db87
f820914768 <820914768@qq.com>
2020-05-01 21:46:14 +0800

    analyze network

--- data/network_analysis.py
+++ data/network_analysis.py
@@ -1,11 +1,20 @@
 # -*- coding: utf-8 -*-
 
 import networkx as nx
+import pandas_datareader.data as web
+import pandas as pd
+import numpy as np
+
+df = pd.read_csv('funds_holding.csv')
+df = df[['stock_title', 'abbrivate_stock']]
 
 
-
-
+def get_average_price(abbrivate, start='2019-1-1'):
+    data = web.DataReader(abbrivate, 'yahoo', start=start)
+    return data['Open'].mean()
 
 
 if __name__ == "__main__":
     g = nx.read_gml('co-investment_network.gml')
+    
+    d = web.DataReader('HII', 'yahoo', start='2019-1-1')
